881c5fd40aba4fc38bc1f9dfd871ff20c41b3fd2
AhmdHosni <ahmdhosny@gmail.com>
2026-03-26 18:19:21 +0200

    updating hyprland install script

--- scripts/configs/hyprland/install-hypr-to-base.sh
+++ scripts/configs/hyprland/install-hypr-to-base.sh
@@ -16,6 +16,15 @@ sudo pacman -S --needed --noconfirm kitty
 ## Notifications
 sudo pacman -S --needed --noconfirm swaync
 
+## Top Toolbar for hyprland
+sudo pacman -S --needed --noconfirm waybar
+
+## A window switcher, application launcher, run dialog, ssh-launcher and dmenu replacement
+sudo pacman -S --needed --noconfirm rofi
+
+## A tool for animated wallpapers (transition). An Answer to your Wayland Wallpaper Woes
+sudo pacman -S --needed --noconfirm swww
+
 ## Grab images from a Wayland compositor. Works great with slurp  for Screen shots
 sudo pacman -S --needed --noconfirm grim
 
@@ -49,9 +58,6 @@ sudo pacman -S --needed --noconfirm wget
 ## Tools allowing to manipulate the Wireless Extensions
 sudo pacman -S --needed --noconfirm wireless_tools
 
-## A window switcher, application launcher, run dialog, ssh-launcher and dmenu replacement
-sudo pacman -S --needed --noconfirm rofi
-
 ## xdg-desktop-portal backend for hyprland
 # This handles screen sharing (OBS, Discord), file pickers, and opening links between apps.
 sudo pacman -S --needed --noconfirm xdg-desktop-portal-hyprland
@@ -66,5 +72,8 @@ sudo pacman -S --needed --noconfirm xdg-utils
 ## Wayland version of xclip for copy and paste
 sudo pacman -S --needed --noconfirm wl-clipboard
 
-## A tool for animated wallpapers (transition). An Answer to your Wayland Wallpaper Woes
-sudo pacman -S --needed --noconfirm swww
+## Theme and Icon Manager
+sudo pacman -S --needed --noconfirm nwg-look
+
+## Applicaiton Dark theme
+sudo pacman -S --needed --noconfirm adw-gtk-theme
